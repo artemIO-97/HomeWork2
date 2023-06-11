6ad7eb89232439541a4c9e0ffcc61701b650f365
artemIO-97 <tipsitip00@gmail.com>
2023-06-12 00:21:15 +0300

    Add temperature

---
+++ Temperature/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Temperature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input temperature: ");
+            var InputTemperature = Console.ReadLine();
+            bool chekInputTemperature = double.TryParse(InputTemperature, out double parseInputTemperature);
+            while (chekInputTemperature == false)
+            {
+                Console.Write("Input error, please try again: ");
+                InputTemperature = Console.ReadLine();
+                chekInputTemperature = double.TryParse(InputTemperature, out parseInputTemperature);
+            }
+            switch (parseInputTemperature)
+            {
+                case <= 0:
+                    Console.WriteLine("Very cold");
+                    break;
+                case <= 10:
+                    if (parseInputTemperature >= 0 && parseInputTemperature <= 10)
+                    {
+                        Console.WriteLine("Cold");
+
+                    }
+                    break;
+                case <= 20:
+                    if (parseInputTemperature >= 10 && parseInputTemperature <= 20)
+                    {
+                        Console.WriteLine("Normal");
+
+                    }
+                    break;
+                case <= 30:
+                    if (parseInputTemperature >= 20 && parseInputTemperature <= 30)
+                    {
+                        Console.WriteLine("Heat");
+
+                    }
+                    break;
+                case >= 30:                 
+                    Console.WriteLine("Very Heat");                   
+                    break;
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
